e8120dc0264b66460aef50f9653ec16185f06ad4
Alurklin <alurklin@gmail.com>
2024-11-06 14:32:48 +0800

    Сериализация
    Добавлен архив в проект

---
+++ EmptyProject3D/EmptyProject/Assets/Scripts/EditorMover.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	
+	[RequireComponent(typeof(PositionSaver))]
+	public class EditorMover : MonoBehaviour
+	{
+		private PositionSaver _save;
+		private float _currentDelay;
+		
+		//todo comment: Что произойдёт, если _delay > _duration?
+		private float _delay = 0.5f;
+		private float _duration = 5f;
+
+		private void Start()
+		{
+			//todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+			//Для оптимизации. Чтоб getcomponent вызывался только 1 раз.
+			_save = GetComponent<PositionSaver>();
+			_save.Records.Clear();
+		}
+
+		private void Update()
+		{
+			_duration -= Time.deltaTime;
+			if (_duration <= 0f)
+			{
+				enabled = false;
+				Debug.Log($"<b>{name}</b> finished", this);
+				return;
+			}
+			
+			//todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+			/*
+			Строка _currentDelay -= Time.deltaTime; эквивалентна записи (_delay -= Time.deltaTime), обе выполняют уменьшение переменной на значение Time.deltaTime.
+			Однако использование отдельной переменной _currentDelay позволяет лучше структурировать код и разделить логику задержки между сохранениями от общей длительности работы скрипта.
+			Это делает код более читаемым и облегчает понимание его работы.
+			*/
+			_currentDelay -= Time.deltaTime;
+			if (_currentDelay <= 0f)
+			{
+				_currentDelay = _delay;
+				_save.Records.Add(new PositionSaver.Data
+				{
+					Position = transform.position,
+					//todo comment: Для чего сохраняется значение игрового времени?
+					Time = Time.time,
+				});
+			}
+		}
+	}
+}
