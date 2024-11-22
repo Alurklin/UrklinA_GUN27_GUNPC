942b7aac2b24b2536d794d37b7691294252667c0
Alurklin <alurklin@gmail.com>
2024-11-22 16:26:37 +0800

    Начало домашки

---
+++ Assets/Edit/FieldOfViewEdit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEdit : Editor
+{
+    private void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.Radius);
+
+        Vector3 viewAngleLeft = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.Angle / 2);
+        Vector3 viewAngleRight = DirectionFromAngle(fov.transform.eulerAngles.y, fov.Angle / 2);
+
+        Handles.color = Color.yellow;
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleLeft * fov.Radius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleRight * fov.Radius);
+
+        private 
+    }
+}
