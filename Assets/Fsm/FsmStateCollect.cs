42005750615a0fb630285da856599bc24e5a3e19
Alurklin <131470750+Alurklin@users.noreply.github.com>
2024-12-11 01:12:27 +0800

    Add fsm
    Добавлена стейт машина

---
+++ Assets/Fsm/FsmStateCollect.cs
@@ -0,0 +1,23 @@
+
+namespace FSM.Scripts
+{
+    public class FsmStateCollect : FsmState
+    {
+        public FsmStateCollect(Fsm fsm) : base(fsm) { }
+
+        public override void Enter()
+        {
+
+        }
+
+        public override void Exit()
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Fsm/FsmStateIdle.cs
@@ -0,0 +1,25 @@
+
+
+namespace FSM.Scripts
+{
+    public class FsmStateIdle : FsmState
+    {
+        public FsmStateIdle (Fsm fsm) : base (fsm) { }
+
+        public override void Enter()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+    }
+}
+
